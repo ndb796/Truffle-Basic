f9d55d67aec8453168ccab80ae20cc1f1fc95820
Dongbin Na <ndb796@naver.com>
2019-07-09 16:29:48 +0900

    Update Project

---
+++ README.md
@@ -0,0 +1,15 @@
+### Installation
+<pre>
+# Install 'MetaMask' to your Chrome browser
+# Install Ganache to your local device
+# Set MetaMask's address to Ganache net.
+npm install
+truffle compile
+# Optional: truffle test
+truffle migrate --reset
+</pre>
+
+### Run
+<pre>
+npm run dev
+</pre>
--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,5 +1,40 @@
 pragma solidity ^0.5.0;
 
 contract HelloWorld {
-    uint public hello = 0;
+    uint public taskCount = 0;
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Study Blockchain");
+    }
+
+    event TaskCompleted (
+        uint id,
+        bool completed
+    );
+
+    event TaskCreated (
+        uint id,
+        string content,
+        bool completed
+    );
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
+    }
 }
